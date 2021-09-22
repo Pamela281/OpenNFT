94edf54ada2e6b20dc1854b98f41874ec100b0a5
Pamela281 <60964329+Pamela281@users.noreply.github.com>
2021-09-22 13:59:55 +0200

    Revert "Update displayFeedback.m and json file with the correct TR number"
    This reverts commit af91ec5046cf84f702c183d63df8d007a553c62a, reversing
    changes made to b65bb8a8b387b6536ccb4b0fc5613876b5b33226.

--- opennft/__main__.py
+++ opennft/__main__.py
@@ -15,8 +15,6 @@ from PyQt5.QtWidgets import QApplication
 from opennft import excepthook, utils
 from opennft import config
 from opennft import opennft
-from opennft import __version__
-from opennft._logging import logging_setup
 
 
 def main():
@@ -27,13 +25,11 @@ def main():
 
     app = QApplication(sys.argv)
 
-    app.setApplicationName(config.APP_NAME)
-    app.setOrganizationName(config.APP_NAME)
-    app.setApplicationVersion(__version__)
+    app.setApplicationName('OpenNFT')
+    app.setOrganizationName('OpenNFT')
+    app.setApplicationVersion('1.0')
 
     config.MATLAB_NAME_SUFFIX = '_{}'.format(utils.generate_random_number_string())
-
-    logging_setup()
 
     nft = opennft.OpenNFT()
     nft.show()
